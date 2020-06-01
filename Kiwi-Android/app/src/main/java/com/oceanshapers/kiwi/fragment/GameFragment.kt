034b46db237c10b5101e7e0818e83cd136b899b3
Anjana Pai <anjanapai2508@gmail.com>
2020-06-01 17:49:01 -0400

    Initial commit of android app

--- Kiwi-Android/app/src/main/java/com/oceanshapers/kiwi/fragment/GameFragment.kt
+++ Kiwi-Android/app/src/main/java/com/oceanshapers/kiwi/fragment/GameFragment.kt
@@ -4,11 +4,11 @@ import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.os.Bundle
+import android.os.Handler
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
@@ -16,12 +16,14 @@ import com.oceanshapers.kiwi.R
 import kotlinx.android.synthetic.main.fragment_game.*
 import java.util.*
 
+
 /**
  * A simple [Fragment] subclass.
  */
 class GameFragment : Fragment() {
     lateinit var plasticAnimation: ObjectAnimator
-    var currentTurtleY = 0f
+
+    //var currentTurtleY = 0f
     var isGameOver = false
     val displayMetrics = DisplayMetrics()
 
@@ -32,40 +34,63 @@ class GameFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false)
     }
+
     // background map order : Budapest -->Budapest - Vienna - Amsterdam - Paris - London
     override fun onStart() {
+        dashboard_text.text = "Budapest" + "\n" + "Fares from $300"
+        val gemImageArray = intArrayOf(
+            R.drawable.vienna, R.drawable.amsterdam, R.drawable.paris, R.drawable.london
+        )
+        val destinationTextArray = arrayOf("Vienna", "Amsterdam", "Paris", "London")
+        fish_image.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        fish_image.buildLayer();
         super.onStart()
-        currentTurtleY = fish_image.y
+        //currentTurtleY = fish_image.y
         jump_up_button.setOnClickListener {
-            fish_image.animate().x(fish_image.x).y(fish_image.y - 200)
+            fish_image.animate().x(fish_image.x).y(fish_image.y - 200).setDuration(200)
         }
         jump_down_button.setOnClickListener {
-            fish_image.animate().x(fish_image.x).y(fish_image.y + 200)
+            fish_image.animate().x(fish_image.x).y(fish_image.y + 200).setDuration(200)
         }
         startLooseLifeTimer()
-        //gemImage.setImageDrawable(resources.getDrawable(R.drawable.gem_blink))
+        val handler = Handler()
+        val runnable: Runnable = object : Runnable {
+            var i = 0
+            override fun run() {
+                gemImage.setImageResource(gemImageArray.get(i))
+                dashboard_text.text = destinationTextArray.get(i) + "\n" + "Fares from $200"
+                i++
+                if (i > gemImageArray.size - 1) {
+                    i = 0
+                }
+                handler.postDelayed(this, 20000)
+            }
+        }
+        handler.postDelayed(runnable, 12000)
         budapest_map.visibility = View.VISIBLE
         vienna_map.visibility = View.VISIBLE
         val budapestVienna = ValueAnimator.ofFloat(1.0f, 0.0f)
-        setAnimationProperties(budapestVienna, vienna_map,budapest_map)
+        setAnimationProperties(budapestVienna, vienna_map, budapest_map)
         budapestVienna.start()
         val viennaAmsterdam = ValueAnimator.ofFloat(1.0f, 0.0f)
-        setAnimationProperties(viennaAmsterdam,amsterdam_map,vienna_map)
+        setAnimationProperties(viennaAmsterdam, amsterdam_map, vienna_map)
         val amsterdamParis = ValueAnimator.ofFloat(1.0f, 0.0f)
-        setAnimationProperties(amsterdamParis,paris_map,amsterdam_map)
+        setAnimationProperties(amsterdamParis, paris_map, amsterdam_map)
         val parisLondon = ValueAnimator.ofFloat(1.0f, 0.0f)
-        setAnimationProperties(parisLondon,london_map,paris_map)
+        setAnimationProperties(parisLondon, london_map, paris_map)
         AnimatorSet().apply {
             play(viennaAmsterdam).after(budapestVienna)
             play(amsterdamParis).after(viennaAmsterdam)
             play(parisLondon).after(amsterdamParis)
             start()
         }
-
     }
 
-    fun setAnimationProperties(animation: ValueAnimator, firstImageView: ImageView,followingImageView: ImageView)
-    {
+    fun setAnimationProperties(
+        animation: ValueAnimator,
+        firstImageView: ImageView,
+        followingImageView: ImageView
+    ) {
         animation.repeatCount = 0
         animation.interpolator = LinearInterpolator()
         animation.duration = 20000L // this will control speed of scrolling
@@ -81,10 +106,10 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
-        startPlasticAnimation(plastic1, -(displayMetrics.widthPixels * 2).toFloat(), 0, 10000)
-        startPlasticAnimation(plastic2, -(displayMetrics.widthPixels * 2).toFloat(), 500, 15000)
-        startPlasticAnimation(plastic3, -(displayMetrics.widthPixels * 2).toFloat(), 1000, 20000)
-        startPlasticAnimation(plastic4, -(displayMetrics.widthPixels * 2).toFloat(), 1500, 25000)
+        startPlasticAnimation(plastic1, -(displayMetrics.widthPixels * 2).toFloat(), 0, 15000)
+        startPlasticAnimation(plastic2, -(displayMetrics.widthPixels * 2).toFloat(), 500, 20000)
+        startPlasticAnimation(plastic3, -(displayMetrics.widthPixels * 2).toFloat(), 10000, 15000)
+        startPlasticAnimation(plastic4, -(displayMetrics.widthPixels * 2).toFloat(), 15000, 18000)
     }
 
     fun startPlasticAnimation(
@@ -102,44 +127,54 @@ class GameFragment : Fragment() {
 
     }
 
-//    private val plasticAnimationListener : Animator.AnimatorListener = object :
-//        Animator.AnimatorListener {
-//        override fun onAnimationRepeat(animation: Animator?) {
-//            plastic1.y = plastic1.y + 50
-//        }
-//
-//        override fun onAnimationEnd(animation: Animator?) {
-//            TODO("Not yet implemented")
-//        }
-//
-//        override fun onAnimationCancel(animation: Animator?) {
-//            TODO("Not yet implemented")
-//        }
-//
-//        override fun onAnimationStart(animation: Animator?) {
-//            System.out.println("Animate plastic")
-//        }
-//
-//    }
-
     fun startLooseLifeTimer() {
         val looseLifeTimer = Timer()
         looseLifeTimer.schedule(object : TimerTask() {
             override fun run() {
                 activity!!.runOnUiThread {
-                    if (((plastic1.x < fish_image.x) && (fish_image.x < plastic1.x + plastic1.width)) && (plastic1.y < fish_image.y && fish_image.y < (plastic1.y + plastic1.height))) {
-                        System.out.println("GAME OVER")
-                        // scrollingBackImageView.stop()
+                    if (detectCollisionWith(plastic1) || detectCollisionWith(plastic2) || detectCollisionWith(
+                            plastic3
+                        ) || detectCollisionWith(plastic4)
+                    ) {
+                       // gameOver.visibility = View.VISIBLE
                     }
                 }
             }
-        }, 10, 10) // Delay of 10 milliseconds for user to get a hang of all game components
+        }, 0, 10) // Delay of 5 milliseconds for user to get a hang of all game components
+    }
+
+    fun detectCollisionWith(plasticImageView: ImageView): Boolean {
+        var collisionDetected = false
+        /*
+         (a,b) ==> (fish.x, fish.y)
+         (a+fishWidth,b) ==> (fish.x + fish.width, fish.y)
+         (a,b+fishHeight) ==> (fish.x, fish.y + fish.length)
+        (a+fishWidth,b+fishHeight) ==> (fish.x + fish.width, fish.y + fish.length)
+
+        (x,y) ==> (plastic.x, plastic.y)
+         (x+plasticWidth,y) ==> (plastic.x + plastic.width, plastic.y)
+         (x,y+plasticHeight) ==> (plastic.x, plastic.y + plastic.length)
+        (x+plasticWidth,y+plasticHeight) ==> (plastic.x + plastic.width, plastic.y + plastic.length)
+
+        //Collison if :
+        1. (a+h > x && a+h<x+h)&& (b+h >y && b+h <y+h)
+        2. (a+w > x && a+w<x+w) && (b> y && b <y+h)
+
+         */
+        if ((plasticImageView.x-20 < fish_image.x) && (fish_image.x < plasticImageView.x-20 + plasticImageView.width) && (plasticImageView.y < fish_image.y) && (fish_image.y < (plasticImageView.y + plasticImageView.height)) ||
+            (plasticImageView.x-20 < fish_image.x + fish_image.width) && (fish_image.x + fish_image.width < plasticImageView.x-20 + plasticImageView.width)
+            &&(plasticImageView.y< fish_image.y+fish_image.height) && (fish_image.y + fish_image.height < plasticImageView.y+plasticImageView.height))
+        {
+            collisionDetected = true
+        }
+        return collisionDetected
     }
 
     override fun onDestroy() {
         super.onDestroy()
         // finalScore = 0
         //startScoring = false
+        fish_image.setLayerType(View.LAYER_TYPE_NONE, null)
         isGameOver = false
     }
 
--- Kiwi-Android/app/src/main/java/com/oceanshapers/kiwi/fragment/ScoresFragment.kt
+++ Kiwi-Android/app/src/main/java/com/oceanshapers/kiwi/fragment/ScoresFragment.kt
@@ -1,20 +1,11 @@
 package com.oceanshapers.kiwi.fragment
 
-import android.animation.Animator
-import android.animation.ValueAnimator
 import android.os.Bundle
-import android.util.DisplayMetrics
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.LinearInterpolator
-
+import androidx.fragment.app.Fragment
 import com.oceanshapers.kiwi.R
-import kotlinx.android.synthetic.main.fragment_game.*
-import kotlinx.android.synthetic.main.fragment_scores.*
-import kotlinx.android.synthetic.main.fragment_scores.background1ImageView
-import kotlinx.android.synthetic.main.fragment_scores.background2ImageView
 
 /**
  * A simple [Fragment] subclass.
@@ -31,29 +22,7 @@ class ScoresFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        val displayMetrics = DisplayMetrics()
-        activity!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
-// % 18 because my tile size is 18px
-// and to avoid partial crop when both image are joined together
-//        val width = displayMetrics.widthPixels
-//        background1ImageView.layoutParams.width = width
-//        background2ImageView.layoutParams.width = width
 
-// start animation
-        val animator = ValueAnimator.ofFloat(1.0f, 0.0f)
-        animator.repeatCount = 0
-        animator.interpolator = LinearInterpolator()
-        animator.duration = 20000L // this will control speed of scrolling
-        animator.addUpdateListener { animation ->
-            val progress = animation.animatedValue as Float
-            val width = budapest_map.getWidth()
-            val translationX = width * progress
-            background1ImageView.setTranslationX(translationX)
-            background2ImageView.setTranslationX(translationX - width)
-            //background3ImageView.setTranslationX(translationX-width)
-        }
-       // animator.addUpdateListener(animatorListener)
-        animator.start()
     }
 
 //    private val animatorListener : ValueAnimator.AnimatorUpdateListener = object : ValueAnimator.AnimatorUpdateListener
